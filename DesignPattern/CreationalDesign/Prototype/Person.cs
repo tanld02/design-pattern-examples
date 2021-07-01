73cf0a364719992f4897f5b994424c53c085d6db
OS <OS@DESKTOP-TENFEDN>
2021-07-01 11:15:33 +0700

    feat: add prototype pattern

---
+++ DesignPattern/CreationalDesign/Prototype/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Person : ICloneable
+    {
+        private readonly string _name;
+        private readonly int _age;
+        public Person(string name, int age)
+        {
+            _name = name;
+            _age = age;
+        }
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello my name is: {_name}, Age: {_age}");
+        }
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ DesignPattern/CreationalDesign/Prototype/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Euro", 2020);
+            Console.WriteLine("Main person: ");
+            person.Introduce();
+            var personCopy = (Person)person.Clone();
+            Console.WriteLine("Copy person: ");
+            personCopy.Introduce();
+        }
+    }
+}
