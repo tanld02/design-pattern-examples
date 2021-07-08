74ca644dd2d27a2a50d883c378b18a3bf0e369ef
tan.le@halliburton.com <tan.le@halliburton.com>
2021-07-08 13:29:54 +0700

    feat: add structure design pattern

---
+++ DesignPattern/StructuralDesign/Facade/Client.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facade
+{
+    class Client
+    {
+        // The client code works with complex subsystems through a simple
+        // interface provided by the Facade. When a facade manages the lifecycle
+        // of the subsystem, the client might not even know about the existence
+        // of the subsystem. This approach lets you keep the complexity under
+        // control.
+        public static void ClientCode(Facade facade)
+        {
+            Console.Write(facade.Operation());
+        }
+    }
+}
